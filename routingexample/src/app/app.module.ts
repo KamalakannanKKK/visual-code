20a4470dfd4fea2c4467c9cb088817a8766c331b
kamalakannanKKK <kamalakannan8765@gmail.com>
2022-09-24 11:20:03 +0530

    practice

--- routingexample/src/app/app.module.ts
+++ routingexample/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HerosListComponent } from './heros-list/heros-list.component';
@@ -13,6 +13,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     
     RouterModule.forRoot([
       {path : 'crisis-list', component : CrisisListComponent},
